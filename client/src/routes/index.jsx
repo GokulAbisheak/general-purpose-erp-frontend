b21df04c8f0e111bb6d072dbdf9081538b4b9537
IT21375132 <gokulabisheak12@gmail.com>
2023-07-19 02:31:23 +0530

    Configured add and update routes

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -13,6 +13,14 @@ import FinanceManagment from "../pages/FinanceManagement";
 import InventoryManagement from "../pages/InventoryManagement";
 import UserLayout from "../layouts/UserLayout";
 import AdminLayout from "../layouts/AdminLayout";
+import AddCustomer from "../pages/CustomerManagment/add";
+import AddEmployee from "../pages/EmployeeManagement/add";
+import AddFinance from "../pages/FinanceManagement/add";
+import AddInventory from "../pages/InventoryManagement/add";
+import UpdateCustomer from "../pages/CustomerManagment/update";
+import UpdateEmployee from "../pages/EmployeeManagement/update";
+import UpdateFinance from "../pages/FinanceManagement/update";
+import UpdateInventory from "../pages/InventoryManagement/update";
 
 const FrontendRoutes = () => {
   return (
@@ -34,6 +42,14 @@ const FrontendRoutes = () => {
             <Route path="/employee" element={<EmployeeManagement />} />
             <Route path="/finance" element={<FinanceManagment />} />
             <Route path="/inventory" element={<InventoryManagement />} />
+            <Route path="/customer/add" element={<AddCustomer />} />
+            <Route path="/employee/add" element={<AddEmployee />} />
+            <Route path="/finance/add" element={<AddFinance />} />
+            <Route path="/inventory/add" element={<AddInventory />} />
+            <Route path="/customer/update" element={<UpdateCustomer />} />
+            <Route path="/employee/update" element={<UpdateEmployee />} />
+            <Route path="/finance/update" element={<UpdateFinance />} />
+            <Route path="/inventory/update" element={<UpdateInventory />} />
           </Route>
         </Routes>
       </Router>
